2ae3cb91fa4cdf005623c3ca4b9a4e2c34c1fa24
heyuaresocute <nastya.gummy@gmail.com>
2023-07-20 11:07:08 +0300

    start to do map page

--- src/app/pages/map-page/map-page.component.ts
+++ src/app/pages/map-page/map-page.component.ts
@@ -4,7 +4,7 @@ import {Router} from "@angular/router";
 @Component({
   selector: 'app-map-page',
   templateUrl: './map-page.component.html',
-  styleUrls: ['./map-page.component.css']
+  styleUrls: ['./map-page.component.scss']
 })
 export class MapPageComponent {
   private _router: Router;
