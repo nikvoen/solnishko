4d9b1aa623cd44b87f4e0f25f2415906d1809fbc
heyuaresocute <nastya.gummy@gmail.com>
2023-07-20 14:27:40 +0300

    rename login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 import { MapPageComponent } from './pages/map-page/map-page.component';
 import { GuessPageComponent } from './pages/guess-page/guess-page.component';
 import { StarsPageComponent } from './pages/stars-page/stars-page.component';
 import {RouterLink, RouterModule, RouterOutlet, Routes} from "@angular/router";
 import { SchedulePageComponent } from './pages/schedule-page/schedule-page.component';
+import { NavigationBarComponent } from './pages/navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginPageComponent,
     ProfilePageComponent,
     MapPageComponent,
     GuessPageComponent,
     StarsPageComponent,
     SchedulePageComponent,
+    NavigationBarComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/pages/navigation-bar/navigation-bar.component.ts
+++ src/app/pages/navigation-bar/navigation-bar.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  selector: 'app-navigation-bar',
+  templateUrl: './navigation-bar.component.html',
+  styleUrls: ['./navigation-bar.component.scss']
 })
-export class LoginPageComponent {
+export class NavigationBarComponent {
   private _router: Router;
   slug: string;
 
